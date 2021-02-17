cea779e4c11a7edfe8e7335e6b113562c025da35
Paulo Souza <pauloogsouza@gmail.com>
2021-02-17 15:54:53 -0300

    Initial implementation

---
+++ src/test/java/com/psouza/springmongodbdemo/SpringMongodbDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psouza.springmongodbdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMongodbDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
