6ff69125b41513525c3771ad28b07273a282c0a4
Paulo Souza <pauloogsouza@gmail.com>
2021-02-17 23:46:37 -0300

    Simple query using query methods

--- src/main/java/com/psouza/springmongodbdemo/repositories/PostRepository.java
+++ src/main/java/com/psouza/springmongodbdemo/repositories/PostRepository.java
@@ -3,5 +3,9 @@ package com.psouza.springmongodbdemo.repositories;
 import com.psouza.springmongodbdemo.entities.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    List<Post> findByTitleContainingIgnoreCase(String text);
 }
