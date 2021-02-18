82deb31304d00f66fa155cba61289c198e7820d8
Paulo Souza <pauloogsouza@gmail.com>
2021-02-17 21:24:00 -0300

    Projection of author data with DTO

--- src/main/java/com/psouza/springmongodbdemo/config/Instantiation.java
+++ src/main/java/com/psouza/springmongodbdemo/config/Instantiation.java
@@ -1,5 +1,6 @@
 package com.psouza.springmongodbdemo.config;
 
+import com.psouza.springmongodbdemo.dto.AuthorDTO;
 import com.psouza.springmongodbdemo.entities.Post;
 import com.psouza.springmongodbdemo.entities.User;
 import com.psouza.springmongodbdemo.repositories.PostRepository;
@@ -33,10 +34,11 @@ public class Instantiation implements CommandLineRunner {
         User alex = new User(null, "Alex Green", "alex@gmail.com", "alex123");
         User bob = new User(null, "Bob Grey", "bob@gmail.com", "bob123");
 
-        Post p1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-        Post p2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
-
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+        Post p1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+        Post p2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
         postRepository.saveAll(Arrays.asList(p1, p2));
     }
 }
